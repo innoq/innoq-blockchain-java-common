735990082fb5aab974d15b083b89b9118c6077e1
Eric Karge <eric.karge@innoq.com>
2018-04-20 11:16:03 +0200

    Do something about stuff

---
+++ blockchain-model/src/main/java/com/innoq/blockchain/java/common/noderegisty/Node.java
@@ -0,0 +1,32 @@
+package com.innoq.blockchain.java.common.noderegisty;
+
+import java.util.Objects;
+
+public class Node {
+
+  public final String noteId;
+  public final String host;
+
+  public Node(String noteId, String host) {
+    this.noteId = noteId;
+    this.host = host;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Node node = (Node) o;
+    return Objects.equals(noteId, node.noteId);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(noteId);
+  }
+}
---
+++ blockchain-model/src/main/java/com/innoq/blockchain/java/common/noderegisty/NodeRegistry.java
@@ -0,0 +1,28 @@
+package com.innoq.blockchain.java.common.noderegisty;
+
+import com.innoq.blockchain.java.common.NodeStatus;
+
+import java.util.*;
+
+public class NodeRegistry {
+  
+  private final String myNodeId;
+  private final Set<Node> neighbours = new HashSet<>();
+
+  public NodeRegistry() {
+    myNodeId = UUID.randomUUID().toString();
+  }
+
+  public void addNode(Node node) {
+    neighbours.add(node);
+  }
+
+  public Collection<Node> getNeighbours() {
+    return neighbours;
+  }
+
+  public NodeStatus getStatus(int blockHeight) {
+    return new NodeStatus(myNodeId, blockHeight, neighbours);
+  }
+
+}
